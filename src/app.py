e07765b3757581716b44cdf70289e66e89feebde
mgomunist <gomunistmariana@gmail.com>
2024-11-25 02:11:22 +0000

    proyecto

---
+++ src/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_text = jsonify(todos)
+    return json_text
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.get_json(force=True)
+    print("Incoming request with the following body", request_body)
+    todos.append(request_body)
+    return jsonify(todos), 200
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    try:        
+        todos.pop(position)
+        return jsonify(todos), 200
+    except IndexError:
+        return jsonify({"error" : "Todo not found"}), 404
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
